f084c240435229c1b87022d867afe58872563799
Madhur Jain <jmadhur.kothadar42@gmail.com>
2023-07-25 23:26:39 +0530

    Time: 9 ms (99.62%), Space: 42.5 MB (95.58%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        
+        int number = x;
+        // This will store the reverse of the number
+        int reverse = 0;
+        while (number > 0) {
+            reverse = reverse * 10 + number % 10;
+            number /= 10;
+        }
+        return x == reverse;
+    }
+}
