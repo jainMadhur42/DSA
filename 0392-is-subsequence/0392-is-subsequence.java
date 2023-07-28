b59d316e0cce7e24859536d3db0c4b4165f27b30
Madhur Jain <jmadhur.kothadar42@gmail.com>
2023-07-28 21:27:52 +0530

    Time: 5 ms (6.5%), Space: 40.5 MB (76.22%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        
+        int j = -1;
+
+        for(int i =0; i< s.length();i++) {
+            char ch = s.charAt(i);
+            j = t.indexOf(ch, j+1);
+            System.out.println("ch "+ch+" j "+j);
+            if(j == -1) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
